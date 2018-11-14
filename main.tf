09669ceae2cc5c02549a23d5a61b3471fa2fc69f
Amin bz <amin.bazaz@etu.umontpellier.fr>
2018-11-14 18:33:27 +0100

    subnet and routes Ok

--- main.tf
+++ main.tf
@@ -11,3 +11,39 @@ resource "aws_s3_bucket" "TD1_intro_devops_bucket"{
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "subnet10" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "eu-west-3a"
+}
+
+resource "aws_subnet" "subnet12" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.12.0/24"
+}
+
+resource "aws_subnet" "subnet13" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.13.0/24"
+}
+
+
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "public_routetable" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_a" {
+  subnet_id      = "${aws_subnet.subnet13.id}"
+  route_table_id = "${aws_route_table.public_routetable.id}"
+}
